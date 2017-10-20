758663298ae82320c9d4ecf2c19cad164919413b
Patrik Marin <me@patrikmarin.fi>
2017-10-20 16:08:22 +0200

    Add cluster coefficient algorithm

--- src/analyser.js
+++ src/analyser.js
@@ -19,3 +19,55 @@ function getTenLeastCloseFriends(nodes) {
     console.log(`${i + 1}: ${sortedNodes[i].name} with ${sortedNodes[i].degree - 1} common friends`)
   }
 }
+
+function getAverageDegree(nodes, links) {
+  const averageDegree = (2 * links.length) / nodes.length;
+  console.log(averageDegree);
+}
+
+class Node {
+  constructor(id) {
+    this.id = id;
+    this.neighbours = [];
+  }
+  
+  addNeighbour(id) {
+    this.neighbours.push(id);
+  }
+}
+
+const nodeList = [];
+const nodeMap = new Map();
+for (node of nodes) {
+  const newNode = new Node(node.id);
+  nodeList.push(newNode);
+  nodeMap.set(node.id, newNode);
+}
+
+for (link of links) {
+  nodeMap.get(link.source).addNeighbour(link.target);
+  nodeMap.get(link.target).addNeighbour(link.source);
+}
+
+function getLocalClusteringCoefficient(id) {
+  
+  const neighbours = nodeMap.get(id).neighbours;
+  if (neighbours.length === 1) return -1;
+  let links = 0;
+  for (w of neighbours) {
+    for (u of neighbours) {
+      if (_.contains(nodeMap.get(w).neighbours, u)) {
+        links += 0.5;
+      }
+    }
+  }
+
+  return (2 * links) / (neighbours.length * (neighbours.length - 1));
+
+}
+
+function getGlobalClusteringCoefficient(nodes) {
+  const clusteringCoefficients = nodes.map(node => getLocalClusteringCoefficient(node.id));
+  const globalClusteringCoefficient = _.reduce(clusteringCoefficients, (a, b) => a + b, 0) / nodes.length;
+  console.log(globalClusteringCoefficient);
+}
